66f037c9d529c8fa4131c9f83d038c094e80aa10
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-09-15 10:56:05 -0500

    Funciones

--- TransIntensidad.m
+++ TransIntensidad.m
@@ -5,4 +5,18 @@ close all;
 %Abrir imagen
 img=imread('IMG/1.jpg');
 
+%Convertimos de uint8 a double
+imgD=double(img);
+
 %Funcion cuadrada
+div=1/255;
+FC=(imgD.^2).*div;
+
+%Funcion cubica
+FCub=(imgD.^3).*(div^2);
+
+%Funcion raíz cuadrada
+FSqrt=sqrt(255.*imgD);
+
+%Funcion logaritmica
+FLog=255.*((log(1+imgD))/(log(1+255)));
